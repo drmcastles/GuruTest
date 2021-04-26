f48081a35abf476fc6dfb2269980c6175b278a48
drmcastles <drmcast@yandex.ru>
2021-04-26 03:33:20 +0300

    models

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 class Test < ApplicationRecord
   has_many :category
   has_many :user, through: :test_passages
+
+  def self.titles_by_category(category)
+    query = "JOIN categories ON tests.category_id = categories.id WHERE categories.title = #{ category }"
+
+    self.joins(query).order('title desc').pluck(:title)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :tests, through: :test_passages
+
+   def test_passage(test)
+    test_passages.order(id: :desc).find_by(test_id: test.id)
+  end
 end
