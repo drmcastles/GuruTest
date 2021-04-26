f77b5217aa62ce6fcfd40212064f408d6c1322d1
drmcastles <drmcast@yandex.ru>
2021-04-26 11:42:20 +0300

    fixing test model

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,11 +1,9 @@
 class Test < ApplicationRecord
-  belongs_to :category
+  belongs_to: category
   has_many :test_passages
   has_many :user, through: :test_passages
 
-  def self.titles_by_category(category)
-    query = "JOIN categories ON tests.category_id = categories.id WHERE categories.title = #{ category }"
-
-    self.joins(query).order('title desc').pluck(:title)
+  def self.titles_by_category(name)
+    joins(:category).where(categories: { title: name }).pluck(:title)
   end
 end
