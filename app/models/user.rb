be8a637cb469172e66c3aeba72ae813cc14a773e
drmcastles <drmcast@yandex.ru>
2021-04-28 09:43:26 +0300

    add associate to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :test_passages
+  has_many :author_tests, class_name: 'Test', foreign_key: :author_id
   has_many :tests, through: :test_passages
   def tests_by_level(level)
 
