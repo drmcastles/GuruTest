d91424f2d5cef43ea941ffe8fa6f563f64c0f48b
drmcastles <drmcast@yandex.ru>
2021-08-14 20:40:15 +0300

    lesson7_fixed

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
 
-  before_action :find_test, only: :index
+  before_action :find_test, only: %i[index create]
   before_action :find_question, only: %i[show destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
@@ -14,24 +14,24 @@ class QuestionsController < ApplicationController
     render json: @question
   end
 
-  def new
-  end
-
   def create
     @test.questions.create!(question_params)
     render plain: 'Question created'
   end
 
   def destroy
-    @question.destroy!
-
+    @question.destroy
     render plain: 'Question deleted'
   end
 
   private
 
+  def question_params
+    params.require(:question).permit(:body)
+  end
+
   def find_test
-    @test = Test.find_by_id(params[:test_id])
+    @test = Test.find(params[:test_id])
   end
 
   def find_question
