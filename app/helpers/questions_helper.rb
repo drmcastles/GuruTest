7baedbdcf1577fd623d1580c05dbf2758652bae4
drmcastles <drmcast@yandex.ru>
2021-08-18 06:14:34 +0300

    fix

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,4 @@
-verb = question.new_record? ? 'New': 'Edit'
-    content_tag(:h1, "#{verb} #{question.test.title.capitalize} Question")  
+module QuestionsHelper
+  verb = question.new_record? ? 'New': 'Edit'
+    content_tag(:h1, "#{verb} #{question.test.title.capitalize} Question")
+end
