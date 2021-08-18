82b09ec21b7cd7a8fde85b6ca67fcdd4d1767795
drmcastles <drmcast@yandex.ru>
2021-08-18 06:33:55 +0300

    fix helper

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,4 +1,6 @@
 module QuestionsHelper
-  verb = question.new_record? ? 'New': 'Edit'
+  def question_header(question)
+    verb = question.new_record? ? 'New': 'Edit'
     content_tag(:h1, "#{verb} #{question.test.title.capitalize} Question")
+  end
 end
