2f4548b1b41c18f7b00d3cfa7374ac7d500e4153
drmcastles <drmcast@yandex.ru>
2021-08-23 15:01:56 +0300

    scaffold answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,19 +1,2 @@
 class Answer < ApplicationRecord
-
-  belongs_to :question
-
-  scope :correct, -> { where correct: true }
-
-  validates :body,
-            :correct,
-            :question_id,
-            presence: true
-
-  validate :validate_amount_answers, on: :create
-
-  def validate_amount_answers
-    return if question.answers.length < 4
-    errors.add(:answers, 'Amount of ansers to question sholud be from 1 to 4')
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :answers
   resources :tests do
     resources :questions, shallow: true
   end
