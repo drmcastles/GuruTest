6edd21be2b6614136814b2c3cae69b500c3b883d
drmcastles <drmcast@yandex.ru>
2021-08-24 11:42:08 +0300

    test passages

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
 
-
-  root 'test#index'
+  root 'tests#index'
 
   resources :tests do
     resources :questions, shallow: true, except: :index do
-      resources :answers, shallow: true
+      resources :answers
+    end
+
+    member do
+      post :start
+    end
+
+  end
+
+  resources :test_passages, only: %i[show update] do
+    member do
+      get :result
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_25_124732) do
+ActiveRecord::Schema.define(version: 2021_08_24_073005) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
